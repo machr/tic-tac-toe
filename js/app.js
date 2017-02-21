47e0f19423354239ae0a1492ec65feb7375d5fdd
Mark Powell <hi@powellmark.com>
2017-02-21 15:44:48 +1100

    merge for deployment

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
-console.log('js connected');
+function createBoard() {
+  var gameboard = document.querySelector('.gameboard');
+  for (var k = 1; k <= 9; k++) {
+    var square = document.createElement('div');
+    square.innerHTML = k;
+    square.classList.add('square');
+    gameboard.appendChild(square);
+  }
+}
+
+createBoard();
