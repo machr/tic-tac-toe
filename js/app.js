43cab5c48cd0dc263d2e298b476f86ea261ec1aa
Mark Powell <hi@powellmark.com>
2017-02-21 19:26:12 +1100

    more logic

--- js/app.js
+++ js/app.js
@@ -1,29 +1,67 @@
-var $gameboard = $('.gameboard');
-var player1 = {
-  name: "Johnny",
-  pick: [] // Push coordinate into array for check
-};
+  var $gameboard = $('.gameboard');
+  var tictactoe = [
+    ['A1A2A3'],['B1B2B3'],['C1C2C3'],
+    ['A1B1C1'],['A2B2C2'],['A3B3C3'],
+   ];
 
-var player2 = {
-  name: "Johnny",
-  pick: [] // Push coordinate into array for check
-};
+  tictactoe =  tictactoe.join(',');
+  console.log(tictactoe);
 
-var turnCounter = 0;
-function getPick(event){
-  var coordinate = $(event.target).data( "coordinate" );
-  $square = $(event.target);
-  if (turnCounter % 2 !== 0) {
-    player1.pick.push(coordinate);
-    $square.addClass( 'blue' );
-  } else {
-    player2.pick.push(coordinate);
-    $square.addClass( 'green' );
+  /**
+  function checkForWin(){
+     if (tictactoe.includes(player1.pick)) {
+       console.log('player1 wins');
+     } else if (tictactoe.includes(player2.pick)) {
+       console.log('player2 wins');
+     }
+   }
+
+  var player1 = {
+    name: "Johnny",
+    pick: [] // Push coordinate into array for check
+  };
+
+  var player2 = {
+    name: "Johnny",
+    pick: [] // Push coordinate into array for check
+  };
+
+
+
+  var turnCounter = 0;
+  function getPick(event){
+    var coordinate = $(event.target).data( "coordinate" );
+    $square = $(event.target);
+    if (turnCounter % 2 !== 0) {
+      player1.pick.push(coordinate);
+      $square.addClass( 'blue' );
+    } else {
+      player2.pick.push(coordinate);
+      $square.addClass( 'green' );
+    }
+    checkForWin();
+    turnCounter++;
+    if (turnCounter > 4) {
+      checkForWin();
+    } else if (turnCounter === 9) {
+      console.log('draw');
+    }
+    console.log(turnCounter);
   }
-  turnCounter++;
-  console.log(turnCounter);
 
-}
 
-// Event listener
-$($gameboard).on('click', 'div', getPick);
+  // compare player's pick array with tictactoe
+
+  // Event listener
+  $($gameboard).on('click', 'div', getPick);
+  // winning combinations (there must be a smarter way!)
+
+
+  var checkForWin = function(){
+    if (tictactoe.includes(player1.pick)) {
+      console.log('player1 wins');
+    } else if (tictactoe.includes(player2.pick)) {
+      console.log('player2 wins');
+    }
+  };
+*/
