98bbcacf1762b784b781b740f454fdec9b95e1bd
Mark Powell <hi@powellmark.com>
2017-02-21 15:33:08 +1100

    board created with JS, scss and browsersync not working yet

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
-console.log('js connected');
+//create row of three
+// function createRow(){
+//   var row = [];
+//   for(var i = 1; i <= 3 ; i++){
+//     row.push(i);
+//   }
+//   return (row);
+// }
+
+
+function createBoard() {
+  var gameboard = document.querySelector('.gameboard');
+  for (var k = 1; k <= 9; k++) {
+    var square = document.createElement('div');
+    square.innerHTML = k;
+    square.classList.add('square');
+    gameboard.appendChild(square);
+  }
+}
+
+createBoard();
