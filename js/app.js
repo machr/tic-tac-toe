0a57f5d6a257cae050403938294c90706aa7bc25
Mark Powell <hi@powellmark.com>
2017-05-04 08:06:53 +1000

    refactored app.js

--- js/app.js
+++ js/app.js
@@ -8,31 +8,31 @@ var winningCombinations = [
   '159','357'
  ];
 
+var player1 = {
+ name: "Kasun",
+ id: 1,
+ picks: []
+};
+var player2 = {
+ name: "DT",
+ id: 2,
+ picks: []
+};
 
- var player1 = {
-   name: "Johnny",
-   id: 1,
-   picks: []
- };
- var player2 = {
-   name: "Bob",
-   id: 2,
-   picks: []
- };
 
- var pickMatching = function (player){
-    for(var i = 0; i < winningCombinations.length; i++) {
-      var combo = winningCombinations[i];
-      var regexPattern = '[' + combo + ']';
-      var regEx = new RegExp(regexPattern, 'g');
-      var matchedNumbers = player.picks.sort().join('').match(regEx);
-      if (matchedNumbers !== null && matchedNumbers.join('') === combo) {
-        hasWon = true;
-        break;
-      }
+var pickMatching = function (player){
+  for(var i = 0; i < winningCombinations.length; i++) {
+    var combo = winningCombinations[i];
+    var regexPattern = '[' + combo + ']';
+    var regEx = new RegExp(regexPattern, 'g');
+    var matchedNumbers = player.picks.sort().join('').match(regEx);
+    if (matchedNumbers !== null && matchedNumbers.join('') === combo) {
+      hasWon = true;
+      break;
     }
-    return hasWon;
- };
+  }
+  return hasWon;
+};
 
 var checkForWin = function(player) {
   if (pickMatching(player)) {
@@ -44,23 +44,27 @@ function getPick(event){
   var coordinate = $(event.target).data( "coordinate" );
   $square = $(event.target);
   if (turnCounter % 2 !== 0) {
-    player1.picks.push(coordinate);
-    $square.addClass( 'player-one' );
-    currentPlayer = player1;
-    $('.gameboard, .square').removeClass( 'glow-purple' );
-    $('.gameboard, .square').addClass( 'glow-blue' );
-
+    currentPlayer = executeTurn(coordinate, 'one', player1);
   } else {
-    player2.picks.push(coordinate);
-    $square.addClass( 'player-two' );
-    currentPlayer = player2;
-    $('.gameboard, .square').removeClass( 'glow-blue' );
-    $('.gameboard, .square').addClass( 'glow-purple' );
+    currentPlayer = executeTurn(coordinate, 'two', player2);
   }
   if (turnCounter >= 5) {
     checkForWin(currentPlayer);
   }
   turnCounter++;
+}
+
+function executeTurn(coord, playerNum, player) {
+  player.picks.push(coord);
+  $square.addClass('player-' + playerNum);
+  if (playerNum == 'one') {
+    colors = ['glow-purple', 'glow-blue'];
+  } else {
+    colors = ['glow-blue', 'glow-purple'];
+  }
+  $('.gameboard, .square').removeClass(colors[0]);
+  $('.gameboard, .square').addClass(colors[1]);
+  return player;
 }
 
 // Event listener
